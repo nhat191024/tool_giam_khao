ffb8fc382596d9d67fffbf7b9892ca96c4645225
Fumino The Fish <richberchannel01@gmail.com>
2024-05-05 15:53:52 +0700

    Update: navbar

--- cham_thi/resources/views/admin/feedback/screen.blade.php
+++ cham_thi/resources/views/admin/feedback/screen.blade.php
@@ -9,13 +9,15 @@
     <title>Document</title>
     <!-- Custom fonts for this template -->
     <link href="{{ url('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ url('css/app.css') }}">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="{{ url('css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+    <!-- Global custom styles -->
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
 
     <!-- Custom styles for this page -->
     <link href="{{ url('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
@@ -25,6 +27,10 @@
     <div class="avatar">
         <p class=" text-white m-auto ">Vũ Chí Thành</p>
         <img src="img/Vu-Chi-Thanh.webp" width="20%" class="avatarImg">
+    </div>
+    <div class="score-nav">
+        <a href="" class="mr-3 text-white">Tổng Hợp</a>
+        <a href="" class="text-white">Chấm Điểm</a>
     </div>
     <div class="logo">
         <img src="img/whiteLogo.png" alt="" width="50%">
