9b4f8a40a786a6d046dd9f9447f91058cfb8f795
nhat191024 <richberchannel01@gmail.com>
2024-08-31 00:27:27 +0700

    chore: Enable encryption for Pusher in broadcasting configuration

--- cham_thi/resources/js/app.js
+++ cham_thi/resources/js/app.js
@@ -6,6 +6,5 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    encrypted: false,
-    forceTLS: false,
+    encrypted: true,
 })
